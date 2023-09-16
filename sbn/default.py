c72a0870730399eb3569117b84d4a825730d38d4
Bart Thate <bthate@dds.nl>
2023-09-16 13:41:29 +0200

    wokrs

---
+++ sbn/default.py
@@ -0,0 +1,21 @@
+# This file is placed in the Public Domain.
+#
+#
+
+
+"configurations"
+
+
+from .objects import Object
+
+
+class Default(Object):
+
+    __default__ = "" 
+
+    def __init__(self):
+        Object.__init__(self)
+        self.__default__ = ""
+
+    def __getattr__(self, key):
+        return self.__dict__.get(key, self.__default__)
