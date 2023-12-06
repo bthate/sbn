7353a06d583f659c3b3c4571e25479aad5a6114f
Bart Thate <bthate@dds.nl>
2023-12-06 16:52:52 +0100

    add repeat

---
+++ sbn/repeat.py
@@ -0,0 +1,73 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C,R,W0718
+
+
+"timers"
+
+
+import threading
+import time
+
+
+from .thread import Thread, launch
+from .timers import Timer
+
+
+def __dir__():
+    return (
+        'Repeater',
+        'Timer',
+        'laps'
+    )
+
+
+__all__ = __dir__()
+    
+
+class Repeater(Timer):
+
+    def run(self) -> Thread:
+        ""
+        thr = launch(self.start)
+        super().run()
+        return thr
+
+
+def laps(seconds, short=True) -> str:
+    txt = ""
+    nsec = float(seconds)
+    if nsec < 1:
+        return f"{nsec:.2f}s"
+    year = 365*24*60*60
+    week = 7*24*60*60
+    nday = 24*60*60
+    hour = 60*60
+    minute = 60
+    years = int(nsec/year)
+    nsec -= years*year
+    weeks = int(nsec/week)
+    nsec -= weeks*week
+    nrdays = int(nsec/nday)
+    nsec -= nrdays*nday
+    hours = int(nsec/hour)
+    nsec -= hours*hour
+    minutes = int(nsec/minute)
+    nsec -= int(minute*minutes)
+    sec = int(nsec)
+    if years:
+        txt += f"{years}y"
+    if weeks:
+        nrdays += weeks * 7
+    if nrdays:
+        txt += f"{nrdays}d"
+    if short and txt:
+        return txt.strip()
+    if hours:
+        txt += f"{hours}h"
+    if minutes:
+        txt += f"{minutes}m"
+    if sec:
+        txt += f"{sec}s"
+    txt = txt.strip()
+    return txt
