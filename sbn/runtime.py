70e185e3b01902a96b9608899d9180c8096e4a51
Bart Thate <bthate@dds.nl>
2023-12-07 16:36:01 +0100

    add runtime

---
+++ sbn/runtime.py
@@ -0,0 +1,56 @@
+# This file is placed in the Public Domain
+#
+#
+
+
+"run"
+
+
+import inspect
+
+
+from .command import Commands
+from .event   import Event
+from .object  import Object
+from .parser  import parse
+from .storage import Storage
+from .thread  import launch
+from .utility import spl, strip
+
+
+def __dir__():
+     return (
+         'command',
+         'scan'
+    )
+
+
+def command(txt):
+    evn = Event()
+    evn.txt = txt
+    parse(evn)
+    Commands.handle(evn)
+    evn.wait()
+    return evn
+
+
+def scan(pkg, modstr, initer=False) -> []:
+    threads = []
+    for modname in spl(modstr):
+        module = getattr(pkg, modname, None)
+        if not module:
+            continue
+        for key, cmd in inspect.getmembers(module, inspect.isfunction):
+            if key.startswith("cb"):
+                continue
+            if 'event' in cmd.__code__.co_varnames:
+                Commands.add(cmd)
+        for key, clz in inspect.getmembers(module, inspect.isclass):
+            if key.startswith("cb"):
+                continue
+            if not issubclass(clz, Object):
+                continue
+            Storage.add(clz)
+        if initer and "init" in dir(module):
+            threads.append(launch(module.init, name=f"init {modname}"))
+    return threads
