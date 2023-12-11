ea3161ddd18d54f5bd18ed8a7bfb6be3d53bdaed
Bart Thate <bthate@dds.nl>
2023-12-11 17:02:44 +0100

    add missing

---
+++ sbn/cache.py
@@ -0,0 +1,32 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C,R,E1101,W0718,W0612,E0611
+
+
+"cache"
+
+
+from .object import Object
+
+
+def __dir__():
+    return (
+            'Cache',
+           )
+
+
+class Cache(Object):
+
+    cache = {}
+
+    @staticmethod
+    def extend(channel, txtlist):
+        if channel not in Cache.cache:
+            Cache.cache[channel] = []
+        Cache.cache[channel].extend(txtlist)
+
+    @staticmethod
+    def size(chan):
+        if chan in Cache.cache:
+            return len(Cache.cache.get(chan, []))
+        return 0
