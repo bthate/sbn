6ce6610e023236bb308b840e376fed143df3f8ff
Nixt Niet <nixtniet@gmail.com>
2026-05-21 20:33:20 +0200

    update package

---
+++ sbn/statics.py
@@ -0,0 +1,95 @@
+# This file is placed in the Public Domain.
+
+
+"tables"
+
+
+NAMES = {
+    "atr": "rss",
+    "cfg": "cfg",
+    "cmd": "bsc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "err": "rss",
+    "exp": "rss",
+    "fie": "fnd",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "lou": "sil",
+    "man": "man",
+    "mbx": "mbx",
+    "mod": "bsc",
+    "nme": "rss",
+    "now": "mdl",
+    "pth": "pth",
+    "pwd": "pwd",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "sil": "sil",
+    "slg": "slg",
+    "srv": "adm",
+    "syn": "rss",
+    "tbl": "adm",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "bsc",
+    "ver": "bsc",
+    "wdr": "adm",
+    "wsd": "wsd"
+}
+
+
+CORE = {
+    "booting": "c32611b1a6da2a123a92fbbb63a1faee",
+    "brokers": "18fc6e381479829fbd822b0dc9380daa",
+    "clients": "0635c558909c6871b0d25ae14ef38b9b",
+    "command": "17198e126ff585d5202ea201da9aae94",
+    "configs": "efb82659a3877aba9bf9200abea87b9b",
+    "defines": "a6803f4eb000da9b7e333c6ea34d47bc",
+    "encoder": "4f4f1beb05e2a662fad03932437532dc",
+    "handler": "6a1f490689aef7eb8a49ef095cd74d39",
+    "message": "fb4a091e8dfe9178f3121684f09ff4bd",
+    "objects": "78aedf3ca2ad7e0d65525b78672b1b48",
+    "package": "38ef5ec7af6ab071ab6ee2cd6ab0a573",
+    "parsers": "2eddfe4ff4d6ed1f1261d8a45a473a7a",
+    "persist": "c0964a7387a64be327cf624f2f128a74",
+    "repeats": "7c8492ab2d848c15c780c3bfde8bd8ce",
+    "runtime": "514dee3706a2435d307518e5286d015b",
+    "threads": "b5fae3b7a0bcf3036cd0aedfb184919d",
+    "utility": "e5f6a7db21e07a5a5945859f68527248"
+}
+
+
+MD5 = {
+    "adm": "4f833eb5e8b1e960caf45544ae47c9e9",
+    "bsc": "075fae7aa53e93df5a7efb38406dadae",
+    "cfg": "68dc5e19bbda27a91444153f03dc7480",
+    "flt": "944d2133caa141bb1e9efca1642dee4e",
+    "fnd": "a24a79a935b8928601c67b5d0120dfbf",
+    "irc": "633c13059f36e4699eb8d266cc24dfd8",
+    "log": "78a2658e5645eea9cec5e7c11691fd22",
+    "man": "17ab9c3c85a777b73a7f00553bb03b25",
+    "mbx": "e8a99dcdb2424304a0bb0c569701388b",
+    "mdl": "89b209eaf28ae0d62f158fba70856ff7",
+    "pth": "421e4f52e779fb130cef5993705a7676",
+    "pwd": "c308a6405f500bbcdde84334fc377bb8",
+    "req": "bc1984d2e9de0310dc1b468f25c7ab8c",
+    "rss": "00829677edf748b2c319cb013a9d417f",
+    "rst": "9b000efecb4c78681824cdd073423129",
+    "sil": "3e4727cee412cfba90b329b537adeef2",
+    "slg": "0e39b144a5716b7df614088d3392d1b8",
+    "tdo": "df0c9d4971ff8933d066253a18b167e7",
+    "thr": "6a3ca26059ccd1e703c4d22e9c347613",
+    "tmr": "3786822a3d96ece1d73853c99e4222e8",
+    "udp": "2bccf9cb0ad921bd56d064955b3f1afa",
+    "web": "4c46b75319c11461d4c482fcbd3da51d",
+    "wsd": "65519d0d45a849164937c59d2134ded1"
+}
